dbf860f306ae19205314dcea68a08a093297e364
YuichiroHatanaka <73636802+YuichiroHatanaka@users.noreply.github.com>
2020-11-30 20:43:50 +0900

    Update Ecount.c

--- Ecount.c
+++ Ecount.c
@@ -8,15 +8,15 @@ int count[26];
 FILE *lf;
 
 int main(void){
-  fopen("test.txt","r");
+  lf = fopen("test.txt","r");
   while((c=fgetc(lf))!=EOF){
     for(i = 0;i < 26;i++){
-      if(c = 65 + i){
+      if(c == 65 + i){
         count[i] = count[i] + 1;
       }
     }
   }
   for(i = 0;i < 26;i++){
-    printf("%c はテキスト内に %d 個ある。", 65+i, count[i]);
+    printf("%c はテキスト内に %d 個ある。\n", 65+i, count[i]);
   }
 }
